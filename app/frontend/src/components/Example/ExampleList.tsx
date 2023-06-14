d2220a6cbbb23838cc6c82aa822cbd8d9aa2cb3c
SebastienDOMME <103254620+SebastienDOMME@users.noreply.github.com>
2023-06-14 21:28:47 +0000

    modified:   app/frontend/index.html 	modified:   app/frontend/src/components/Example/ExampleList.tsx 	modified:   app/frontend/src/pages/chat/Chat.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Question 1 ?",
+        value: "Question 1 ?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Question 2 ?", value: "Question 2 ?" },
+    { text: "Question 3 ?", value: "Question 3 ?" }
 ];
 
 interface Props {
